a1f12a3d897b106691716f5b2980a5ab4b73438e
Abdullah Khetran <khetranabdullah@gmail.com>
2024-01-13 21:20:57 +0500

    parameter in the URL path

--- chapter_3/hello.py
+++ chapter_3/hello.py
@@ -3,9 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/hi")
-def greet():
-    return "Hello? World?"
+# @app.get("/hi")
+# def greet():
+#     return "Hello? World?"
+
+# URL Path
+@app.get("/hi/{who}")
+def greet(who: str):
+    return f"Hello {who.capitalize()}"
 
 
 # Start Uvicorn internally
