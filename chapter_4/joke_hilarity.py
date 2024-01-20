0fa7805f63ca30e8f12528bdf049d94f6168ad4d
Abdullah Khetran <khetranabdullah@gmail.com>
2024-01-20 19:32:14 +0500

    python asyncio

---
+++ chapter_4/joke_hilarity.py
@@ -0,0 +1,16 @@
+import asyncio
+
+
+async def q():
+    print("Why cant programmers tell jokes?")
+    await asyncio.sleep(3)
+
+
+async def a():
+    print("Timing!")
+
+
+async def main():
+    await asyncio.gather(q(), a())
+
+asyncio.run(main())
