b1de378fd7a6a841c9800f5ae844ccbe8d09e082
Abdullah Khetran <khetranabdullah@gmail.com>
2024-01-20 19:39:07 +0500

    FastAPI and Async

---
+++ chapter_4/greet_async.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import asyncio
+
+app = FastAPI()
+
+
+@app.get("/hi")
+async def greet():
+    await asyncio.sleep(1)
+    return "Hello? World?"
+
+# start uvicorn usign this command: uvicorn greet_async:app
+
+# The only difference from a synchronous function that used the standard
+# sleep(1) function is that the web server can handle other requests in the
+# meantime with the async example.
