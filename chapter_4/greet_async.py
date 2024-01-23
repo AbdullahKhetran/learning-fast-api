104c407d9847d0729b9f2b38ec5a4197539d1eb3
Abdullah Khetran <khetranabdullah@gmail.com>
2024-01-23 22:23:34 +0500

    corrected typo

--- chapter_4/greet_async.py
+++ chapter_4/greet_async.py
@@ -9,7 +9,7 @@ async def greet():
     await asyncio.sleep(1)
     return "Hello? World?"
 
-# start uvicorn usign this command: uvicorn greet_async:app
+# start uvicorn using this command: uvicorn greet_async:app
 
 # The only difference from a synchronous function that used the standard
 # sleep(1) function is that the web server can handle other requests in the
