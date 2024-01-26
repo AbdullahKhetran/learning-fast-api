1fd3cc8cbb3d9effc9487834bec7520295b63003
Abdullah Khetran <khetranabdullah@gmail.com>
2024-01-26 23:23:38 +0500

    path decorater

--- chapter_6/dependency.py
+++ chapter_6/dependency.py
@@ -18,3 +18,17 @@ def user_dep(name: str = Params, password: str = Params):
 @app.get("/user")
 def get_user(user: dict = Depends(user_dep)) -> dict:
     return user
+
+
+# Path decorater in preceding line
+# in case dependency funtion just checks something and doesn't return any values
+
+def check_dep(name: str = Params, password: str = Params):
+    if not name:
+        raise
+
+
+# path decorater
+@app.get("/check_user", dependencies=[Depends(check_dep)])
+def check_user() -> bool:
+    return True
