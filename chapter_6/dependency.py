a7ecea8a9f1a8bc8b1d2c8fd2c781d226e9db784
Abdullah Khetran <khetranabdullah@gmail.com>
2024-01-26 23:28:26 +0500

    global scope

--- chapter_6/dependency.py
+++ chapter_6/dependency.py
@@ -19,9 +19,14 @@ def user_dep(name: str = Params, password: str = Params):
 def get_user(user: dict = Depends(user_dep)) -> dict:
     return user
 
+# Dependency has 3 scopes:
+#   Single Path
+#   Multiple Paths
+#   Global
 
 # Path decorater in preceding line
 # in case dependency funtion just checks something and doesn't return any values
+
 
 def check_dep(name: str = Params, password: str = Params):
     if not name:
@@ -32,3 +37,24 @@ def check_dep(name: str = Params, password: str = Params):
 @app.get("/check_user", dependencies=[Depends(check_dep)])
 def check_user() -> bool:
     return True
+
+
+# Global
+# when defining top-level FastAPI application object,
+# multiple dependencies can be added which will be applied to all its path functions
+
+def depfunc1():
+    pass
+
+
+def depfunc2():
+    pass
+
+
+# global scoped dependencies
+app = FastAPI(dependencies=[Depends(depfunc1), Depends(depfunc2)])
+
+
+@app.get("/main")
+def fet_main():
+    pass
