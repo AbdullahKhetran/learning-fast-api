19cc2dcc6e44c38c8c7126ac34dd82c53ab5b5b8
Abdullah Khetran <khetranabdullah@gmail.com>
2024-01-31 22:19:59 +0500

    endpoint using path parameter

--- part_iii/src/main.py
+++ part_iii/src/main.py
@@ -8,7 +8,13 @@ def top():
     return "top here"
 
 
+@app.get("/echo/{thing}")
+def echo(thing):
+    return f"echoing {thing}"
+
+
 # starting uvicorn
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", reload=True)
+# run command `python main.py` in this directory
