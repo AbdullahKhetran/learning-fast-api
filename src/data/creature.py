11b7e6226f61fce236308a7f8d7fb959fcbb0fb3
Abdullah Khetran <khetranabdullah@gmail.com>
2024-02-06 19:59:01 +0500

    database connection at startup time

--- src/data/creature.py
+++ src/data/creature.py
@@ -1,14 +1,22 @@
-import sqlite3
+from .init import conn, curs
 from model.creature import Creature
 
-DB_NAME = "cryptid.db"
-conn = sqlite3.connect(DB_NAME)
-curs = conn.cursor()
+# DB_NAME = "cryptid.db"
+# conn = sqlite3.connect(DB_NAME)
+# curs = conn.cursor()
 
 
-def init():
-    curs.execute(
-        "create table creature(name, description, country, area, aka)")
+# def init():
+#     curs.execute(
+#         "create table creature(name, description, country, area, aka)")
+
+
+curs.execute("""create table if not exists creature(
+             name text primary key,
+             description text,
+             country text,
+             area text,
+             aka text)""")
 
 
 def row_to_model(row: tuple) -> Creature:
@@ -41,17 +49,25 @@ def create(creature: Creature):
     (:name, :description, :country, :area, :aka)"""
     params = model_to_dict(creature)
     curs.execute(qry, params)
+    return get_one(creature.name)
 
 
-def modify(creature: Creature):
-    return creature
+def modify(creature: Creature) -> Creature:
+    qry = """update creature
+             set country:country,
+                 name=:name,
+                 description=:description,
+                 area=:area,
+                 aka=:aka
+            where name=:name_orig"""
+    params = model_to_dict(creature)
+    params["name_orig"] = creature.name
+    _ = curs.execute(qry, params)
+    return get_one(creature.name)
 
 
-def replace(creature: Creature):
-    return creature
-
-
-def delete(creature: Creature):
+def delete(creature: Creature) -> bool:
     qry = "delete from creature where name = :name"
     params = {"name": creature.name}
-    curs.execute(qry, params)
+    res = curs.execute(qry, params)
+    return bool(res)
