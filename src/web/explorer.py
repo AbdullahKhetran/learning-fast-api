1da277360b7e898745a4df860aac1b6751499524
Abdullah Khetran <khetranabdullah@gmail.com>
2024-02-09 22:32:10 +0500

    handling errors

--- src/web/explorer.py
+++ src/web/explorer.py
@@ -1,40 +1,46 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from model.explorer import Explorer
-# import fake.explorer as service
 import data.explorer as service
+# from service import explorer as service
+from errors import Duplicate, Missing
 
 router = APIRouter(prefix="/explorer")
 
 
-# @router.get("")
+@router.get("")
 @router.get("/")
 def get_all() -> list[Explorer]:
     return service.get_all()
 
 
 @router.get("/{name}")
-def get_one(name) -> Explorer | None:
-    return service.get_one(name)
+def get_one(name) -> Explorer:
+    try:
+        return service.get_one(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
 
 
-# TODO: make these functional
-# these endpoints do nothing yet
-# @router.post("")
+@router.post("")
 @router.post("/")
 def create(explorer: Explorer) -> Explorer:
-    return service.create(explorer)
+    try:
+        return service.create(explorer)
+    except Duplicate as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
 
 
 @router.patch("/")
-def modify(explorer: Explorer) -> Explorer:
-    return service.modify(explorer)
+def modify(name: str, explorer: Explorer) -> Explorer:
+    try:
+        return service.modify(name, explorer)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
 
 
-@router.put("/")
-def replace(explorer: Explorer) -> Explorer:
-    return service.replace(explorer)
-
-
-@router.delete("/{name}")
+@router.delete("/{name}", status_code=204)
 def delete(name: str):
-    return None
+    try:
+        return service.delete(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
