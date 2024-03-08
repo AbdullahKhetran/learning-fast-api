4870573816d9ff6edb28ca2f457e5c06d8296ef1
Abdullah Khetran <khetranabdullah@gmail.com>
2024-03-08 16:37:35 +0500

    using simple llm

---
+++ src/ai.py
@@ -0,0 +1,23 @@
+from transformers import (AutoTokenizer,
+                          AutoModelForSeq2SeqLM, GenerationConfig)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+model_name = "google/flan-t5-base"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+config = GenerationConfig(max_new_tokens=200)
+
+
+@app.get("/ai")
+def prompt(line: str) -> str:
+    tokens = tokenizer(line, return_tensors="pt")
+    outputs = model.generate(**tokens,
+                             generator_config=config)
+    result = tokenizer.batch_decode(outputs,
+                                    skip_special_tokens=True)
+    return result[0]
+
+# run `uvicorn ai:app` to start
