8c4b76bf4dfcec4435786fe93490781fae802b7d
Abdullah Khetran <khetranabdullah@gmail.com>
2024-03-11 23:15:33 +0500

    downloading using StreamingResponse for large files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, StreamingResponse
 from web import explorer, creature, user
+from pathlib import Path
+from typing import Generator
+
 
 app = FastAPI()
 
@@ -10,7 +13,7 @@ app.include_router(creature.router)
 # app.include_router(user.router)
 
 
-# Uploading Files
+# --- Uploading Files ---
 @app.post("/small")
 async def upload_small_file(small_file: bytes = File()) -> str:
     return f"file size: {len(small_file)}"
@@ -22,10 +25,27 @@ async def upload_big_file(big_file: UploadFile) -> str:
     return f"file size: {big_file.size}, name: {big_file.filename}"
 
 
-# Downloading Files
+# --- Downloading Files ---
 @app.get("/small/{name}")
 async def download_small_file(name):
     return FileResponse(name)
+
+
+# Streaming Response for large files
+def gen_file(path: str) -> Generator:
+    with open(file=path, mode="rb") as file:
+        yield file.read()
+
+
+@app.get("/download_big/{name}")
+async def download_big_file(name: str):
+    gen_expr = gen_file(name)
+    response = StreamingResponse(
+        content=gen_expr,
+        status_code=200,
+    )
+    return response
+
 
 # starting uvicorn
 if __name__ == "__main__":
